4c14bb1370282fa8db504b4e181deedb95f946f5
soybean33 <80668684+soybean33@users.noreply.github.com>
2023-09-06 10:48:43 +0900

    init: 프로젝트 초기 생성

---
+++ app/src/main/java/com/a406/horsebit/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.a406.horsebit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
