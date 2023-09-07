fe1cc9ba4a72a0b4f18b9d9b756eae5f230d3d8f
HHongmoris <moris8470@gmail.com>
2023-09-07 15:23:47 +0900

    feat: HorseBit 프로젝트 생성

---
+++ Backend/horsebit/src/test/java/com/a406/horsebit/HorsebitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a406.horsebit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HorsebitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
