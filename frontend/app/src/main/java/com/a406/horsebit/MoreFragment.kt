7500e2241dcd27c71ecc0db94cf39670e452df51
mintae <alsxo824@naver.com>
2023-09-12 15:25:07 +0900

    [Feat] 1. 더보기 페이지 구조 완성

--- frontend/app/src/main/java/com/a406/horsebit/MoreFragment.kt
+++ frontend/app/src/main/java/com/a406/horsebit/MoreFragment.kt
@@ -1,5 +1,7 @@
 package com.a406.horsebit
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -15,6 +17,19 @@ class MoreFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_more, container, false)
 
         binding = FragmentMoreBinding.bind(view)
+
+
+        // lih_Notice LinearLayout에 클릭 리스너를 추가
+        binding.lihNotice.setOnClickListener {
+            // 웹 페이지로 이동할 URL 정의
+            val websiteUrl = "https://m.kra.co.kr/park/jeju/parkNoticeList.do"
+
+            // 웹 브라우저를 열기 위한 인텐트 생성
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(websiteUrl))
+
+            // 액티비티를 실행하여 웹 페이지로 이동
+            startActivity(intent)
+        }
 
         return view
     }
