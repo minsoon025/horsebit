d87b0a5360b8776c686534a14e96b0799d330966
ehdghkdud159 <ehdghkdud159@hanmail.net>
2023-09-12 17:34:50 +0900

    [CHORE] 프로젝트 기본 패키지 구조 및 파일 생성

---
+++ Backend/horsebit/src/main/java/com/a406/horsebit/controller/TokenController.java
@@ -0,0 +1,4 @@
+package com.a406.horsebit.controller;
+
+public class TokenController {
+}
---
+++ Backend/horsebit/src/main/java/com/a406/horsebit/service/TradeServiceImpl.java
@@ -0,0 +1,4 @@
+package com.a406.horsebit.service;
+
+public class TradeServiceImpl implements TradeService {
+}
