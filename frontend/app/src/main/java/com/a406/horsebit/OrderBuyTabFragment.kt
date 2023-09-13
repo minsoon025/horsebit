c31bd310c056b10ccedd76baaa0085eddf0653a5
soybean33 <80668684+soybean33@users.noreply.github.com>
2023-09-13 14:22:44 +0900

    [FEAT] 매수 완성

--- frontend/app/src/main/java/com/a406/horsebit/OrderBuyTabFragment.kt
+++ frontend/app/src/main/java/com/a406/horsebit/OrderBuyTabFragment.kt
@@ -5,17 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import com.a406.horsebit.databinding.FragmentOrderBuyTabBinding
 import com.a406.horsebit.databinding.FragmentStockOrderBinding
 
 class OrderBuyTabFragment : Fragment() {
 
     private lateinit var binding : FragmentOrderBuyTabBinding
+    private lateinit var orderNum: Spinner
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_order_buy_tab, container, false)
 
         binding = FragmentOrderBuyTabBinding.bind(view)
+
 
         return view
     }
