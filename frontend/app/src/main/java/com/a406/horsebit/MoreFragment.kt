6d4b6e1b651bdb623b39d25371dca42a261e2396
mintae <alsxo824@naver.com>
2023-09-19 15:46:16 +0900

    [FEAT] 1. 더보기 페이지 기능 추가

--- frontend/app/src/main/java/com/a406/horsebit/MoreFragment.kt
+++ frontend/app/src/main/java/com/a406/horsebit/MoreFragment.kt
@@ -1,6 +1,7 @@
 import android.app.AlertDialog
 import android.content.Intent
 import android.graphics.Color
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
@@ -13,6 +14,9 @@ import androidx.biometric.BiometricPrompt
 import androidx.biometric.BiometricPrompt.PromptInfo
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import com.a406.horsebit.MyEditFragment
+import com.a406.horsebit.R
 import com.a406.horsebit.databinding.FragmentMoreBinding
 
 class MoreFragment : Fragment() {
@@ -29,6 +33,51 @@ class MoreFragment : Fragment() {
     ): View? {
         binding = FragmentMoreBinding.inflate(inflater, container, false)
         val view = binding.root
+        // lih_EditMe LinearLayout에 클릭 리스너를 추가
+        binding.lihEditMe.setOnClickListener {
+
+            // FragmentTransaction을 시작하여 화면 전환
+            val transaction: FragmentTransaction = requireActivity().supportFragmentManager.beginTransaction()
+            val ft = requireActivity().supportFragmentManager.beginTransaction()
+
+            val myEditFragment = MyEditFragment()
+
+            // FragmentTransaction을 시작하여 화면 전환
+            ft.replace(R.id.fl_MainFrameLayout, myEditFragment) // R.id.fl_MainFrameLayout,  Fragment를 표시할 레이아웃 컨테이너의 ID입니다.
+            ft.addToBackStack(null) // 뒤로 가기 버튼을 누를 때 이전 Fragment로 이동할 수 있도록 스택에 추가
+            ft.commit()
+        }
+
+
+        // lih_Notice LinearLayout에 클릭 리스너를 추가
+        binding.lihNotice.setOnClickListener {
+            // 웹 페이지로 이동할 URL 정의
+            val websiteUrl = "https://m.kra.co.kr/park/jeju/parkNoticeList.do"
+
+            // 웹 브라우저를 열기 위한 인텐트 생성
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(websiteUrl))
+
+            // 액티비티를 실행하여 웹 페이지로 이동
+            startActivity(intent)
+
+            // lih_InOut LinearLayout에 클릭 리스너를 추가
+            binding.lihInOut.setOnClickListener {
+
+                // FragmentTransaction을 시작하여 화면 전환
+                val transaction: FragmentTransaction = requireActivity().supportFragmentManager.beginTransaction()
+                val ft = requireActivity().supportFragmentManager.beginTransaction()
+
+                val exchangeFragment = ExchangeFragment()
+
+                ft.replace(R.id.fl_MainFrameLayout, exchangeFragment)
+                ft.addToBackStack(null) // 백 스택에 추가하면 뒤로 가기 버튼으로 이전 프래그먼트로 이동 가능
+                ft.commit()
+
+
+
+
+            }
+        }
 
         // 초기 상태에서 "불가능" 텍스트의 색상을 빨간색으로 설정
         binding.tvTradePossible.text = "불가"
