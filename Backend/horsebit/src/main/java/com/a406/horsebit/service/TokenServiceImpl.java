9aecf05ac5fbce1d345cfd097a4e97221f2d6c7e
HwayeongDong <ehdghkdud159@hanmail.net>
2023-09-22 18:09:42 +0900

    [TEST] 토큰 목록 전체 조회 테스트 코드 추가
    삭제 예정

--- Backend/horsebit/src/main/java/com/a406/horsebit/service/TokenServiceImpl.java
+++ Backend/horsebit/src/main/java/com/a406/horsebit/service/TokenServiceImpl.java
@@ -1,5 +1,6 @@
 package com.a406.horsebit.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +25,20 @@ public class TokenServiceImpl implements TokenService {
 	@Override
 	public List<TokenDTO> getAllTokens() {
 		List<TokenDTO> result = tokenRepository.findAllTokens();
+
+		//TODO: 아래의 테스트코드 삭제 요망
+		List<Double> testTrend = new ArrayList<>();
+		testTrend.add(-22.3);
+		testTrend.add(-19.3);
+		testTrend.add(-15.0);
+		testTrend.add(0.0);
+		testTrend.add(1.0);
+		testTrend.add(17.0);
+		testTrend.add(23.8);
+		int i = 0;
+		for(TokenDTO token : result) {
+			token.setPriceTrend(testTrend.get(i++));
+		}
 
 		log.debug("TokenServiceImpl::getAllTokens() START" + result.toString());
 
