6e2caf738ef8381cc45008a1b79f40d0022a7a33
h78749891 <h78749891@gmail.com>
2023-09-25 15:02:41 +0900

    [FEAT] #S09P22A406-201/202 1. Add PriceSevice PriceRateOfChange methods(4)

--- Backend/horsebit/src/main/java/com/a406/horsebit/service/PriceServiceImpl.java
+++ Backend/horsebit/src/main/java/com/a406/horsebit/service/PriceServiceImpl.java
@@ -1,11 +1,13 @@
 package com.a406.horsebit.service;
 
 import com.a406.horsebit.dto.PriceDTO;
+import com.a406.horsebit.dto.PriceRateOfChangeDTO;
 import com.a406.horsebit.repository.redis.PriceRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
@@ -26,5 +28,36 @@ public class PriceServiceImpl implements PriceService {
     @Override
     public List<PriceDTO> getCurrentPrice(List<Long> tokenNoList) {
         return priceRepository.findAllCurrentPriceByTokenNo(tokenNoList);
+    }
+
+    @Override
+    public PriceRateOfChangeDTO getPriceOfRate(Long tokenNo, Long currentPrice) {
+        double doubleStartPrice = priceRepository.findOneStartPriceByTokenNo(tokenNo).getPrice().doubleValue();
+        return new PriceRateOfChangeDTO((currentPrice.doubleValue() - doubleStartPrice) / doubleStartPrice);
+    }
+
+    @Override
+    public PriceRateOfChangeDTO getPriceOfRate(Long tokenNo) {
+        Long currentPrice = priceRepository.findOneCurrentPriceByTokenNo(tokenNo).getPrice();
+        return getPriceOfRate(tokenNo, currentPrice);
+    }
+
+    @Override
+    public List<PriceRateOfChangeDTO> getPriceOfRate(List<Long> tokenNoList, List<PriceDTO> currentPriceDTOList) {
+        List<PriceDTO> startPriceDTOList = priceRepository.findAllStartPriceByTokenNo(tokenNoList);
+        List<PriceRateOfChangeDTO> doublePriceRateOfChangeList = new ArrayList<PriceRateOfChangeDTO>(currentPriceDTOList.size());
+        int index = 0;
+        for (PriceDTO currentPriceDTO: currentPriceDTOList) {
+            double currentPrice = currentPriceDTO.getPrice().doubleValue();
+            double startPrice = startPriceDTOList.get(index).getPrice().doubleValue();
+            doublePriceRateOfChangeList.add(new PriceRateOfChangeDTO((currentPrice - startPrice) / startPrice));
+        }
+        return doublePriceRateOfChangeList;
+    }
+
+    @Override
+    public List<PriceRateOfChangeDTO> getPriceOfRate(List<Long> tokenNoList) {
+        List<PriceDTO> currentPriceDTOList = priceRepository.findAllCurrentPriceByTokenNo(tokenNoList);
+        return getPriceOfRate(tokenNoList, currentPriceDTOList);
     }
 }
