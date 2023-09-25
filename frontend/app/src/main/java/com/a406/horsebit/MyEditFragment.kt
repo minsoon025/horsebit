3c92bf5439aad228e17ece9b38fee7cddd427dcc
mintae <alsxo824@naver.com>
2023-09-25 10:41:07 +0900

    [FEAT] 1. 이름 수정 팝업창

--- frontend/app/src/main/java/com/a406/horsebit/MyEditFragment.kt
+++ frontend/app/src/main/java/com/a406/horsebit/MyEditFragment.kt
@@ -10,18 +10,49 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.EditText
 import android.widget.Spinner
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.a406.horsebit.databinding.FragmentMyEditBinding
-import com.google.android.material.button.MaterialButton
 
 class MyEditFragment : Fragment() {
 
     private lateinit var binding: FragmentMyEditBinding
     private lateinit var spinnerBankOrSecurities: Spinner
     private lateinit var tvSend: TextView
+    private fun showEditNamePopup() {
+        // 팝업창을 위한 레이아웃을 가져옵니다.
+        val popupView = LayoutInflater.from(requireContext()).inflate(R.layout.popup_edit_name, null)
+
+        // 팝업창 내부의 뷰를 참조합니다.
+        val editNameEditText = popupView.findViewById<EditText>(R.id.editNameEditText)
+        val confirmButton = popupView.findViewById<Button>(R.id.confirmButton)
+        val cancelButton = popupView.findViewById<Button>(R.id.cancelButton)
+
+        // AlertDialog를 생성하고 팝업창을 설정합니다.
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setView(popupView)
+        val alertDialog = builder.create()
+
+        // "확인" 버튼 클릭 이벤트 처리
+        confirmButton.setOnClickListener {
+            val newName = editNameEditText.text.toString()
+            // 여기에서 새로운 이름을 처리하고 저장할 수 있습니다.
+            // 예: binding.tvEditName.text = "현재 이름: $newName"
+            alertDialog.dismiss() // 팝업창을 닫습니다.
+        }
+
+        // "취소" 버튼 클릭 이벤트 처리
+        cancelButton.setOnClickListener {
+            alertDialog.dismiss() // 팝업창을 닫습니다.
+        }
+
+        // 팝업창을 표시합니다.
+        alertDialog.show()
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -34,23 +65,7 @@ class MyEditFragment : Fragment() {
 
         // 이름 변경 팝업 / 일단 안됨
         binding.btnEditName.setOnClickListener {
-            AlertDialog.Builder(requireContext())
-                .setView(R.layout.fragment_my_edit)
-                .show()
-                .also { alertDialog ->
-
-                    if (alertDialog == null) {
-                        return@also
-                    }
-
-                    val userName = alertDialog.findViewById<EditText>(R.id.tv_EditName)?.text
-                    val button = alertDialog.findViewById<MaterialButton>(R.id.btn_EditName)
-
-                    button?.setOnClickListener {
-                        alertDialog.dismiss()
-                        Log.d("MyTag", "userName : $userName")
-                    }
-                }
+            showEditNamePopup()
         }
 
 
@@ -122,6 +137,7 @@ class MyEditFragment : Fragment() {
                 return view
             }
         }
+
 
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinnerBankOrSecurities.adapter = adapter
