a5a2ff054e4774ec7d1adec88bd410a8c1fdd212
soybean33 <80668684+soybean33@users.noreply.github.com>
2023-09-26 15:07:12 +0900

    [FIX] 그래프 크기 변하는 문제 해결

--- frontend/app/src/main/java/com/a406/horsebit/StockOrderFragment.kt
+++ frontend/app/src/main/java/com/a406/horsebit/StockOrderFragment.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.a406.horsebit.databinding.FragmentHomeBinding
 import com.a406.horsebit.databinding.FragmentStockOrderBinding
 
 class StockOrderFragment : Fragment() {
