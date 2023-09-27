9bc18dbcfb7f80f1cca6fd79d169016e008caedb
h78749891 <h78749891@gmail.com>
2023-09-27 11:10:36 +0900

    [FIX] 1. Allow API permit

--- Backend/horsebit/src/main/java/com/a406/horsebit/config/WebOAuthSecurityConfig.java
+++ Backend/horsebit/src/main/java/com/a406/horsebit/config/WebOAuthSecurityConfig.java
@@ -80,7 +80,7 @@ public class WebOAuthSecurityConfig {
                 .authorizeHttpRequests(
                         authorize -> authorize
                                 .requestMatchers("/*/*/auth/**").permitAll()
-                                .requestMatchers("/api/**").authenticated()
+                                .requestMatchers("/api/**").permitAll()
                                 .anyRequest().permitAll()
                 )
                 .csrf(AbstractHttpConfigurer::disable)
