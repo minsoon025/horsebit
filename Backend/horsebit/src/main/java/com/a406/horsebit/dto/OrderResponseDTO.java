66ccd21924351893e3d2c49bd9e6dbe0361e4558
h78749891 <h78749891@gmail.com>
2023-10-04 08:29:35 +0900

    [MOD] 1. Add constructor to order response dto

--- Backend/horsebit/src/main/java/com/a406/horsebit/dto/OrderResponseDTO.java
+++ Backend/horsebit/src/main/java/com/a406/horsebit/dto/OrderResponseDTO.java
@@ -1,8 +1,10 @@
 package com.a406.horsebit.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+@AllArgsConstructor
 @Getter
 @Setter
 public class OrderResponseDTO {
